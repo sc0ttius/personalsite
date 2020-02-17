bad2fad5d18c4ee4c90339d731596a0f4421c0d7
Scott Gustas <scottius@gmail.com>
2020-02-17 00:04:26 -0500

    Check for window due to webpack build error on netlify part deux

--- src/components/themeContext.js
+++ src/components/themeContext.js
@@ -8,12 +8,12 @@ export const useTheme = () => React.useContext(ThemeToggleContext);
 const ToggleThemeProvider = ({ children }) => {
 
   const [themeState, setThemeState] = React.useState({
-    mode: ( window && window.localStorage.getItem('mode') ) || 'light'
+    mode: ( typeof window !== 'undefined' && window && localStorage.getItem('mode') ) || 'light'
   });
 
   const toggle = () => {
     const mode = (themeState.mode === 'light' ? `dark` : `light`);
-    if ( window ) window.localStorage.setItem('mode', mode);
+    if ( typeof window !== 'undefined' && window ) localStorage.setItem('mode', mode);
     setThemeState({ mode: mode });
   };
 
